93a265dfc1ea7aed378e6035eb1825334b708672
cammatth2001 <46729770+cammatth2001@users.noreply.github.com>
2019-02-04 17:27:26 -0600

    Update to CS Scripts
    Add file calls for XML and update the CS game for Quiz

--- Quiz-Game/Assets/GameManager.cs
+++ Quiz-Game/Assets/GameManager.cs
@@ -1,16 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using System.Xml;
 
 public class GameManager : MonoBehaviour {
 
+	public Question[] questions;
+
+	//Read Question True or False into the Questions List
+
+	
+	private static List<Question> unansweredQuestions;
+	private Question currentQuestion;
+	[SerializeField]
+	private Text questionText;
+
+	[SerializeField]
+	private float timeBetweenQuestions = 1F;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		if(unansweredQuestions == null || unansweredQuestions.Count == 0)
+		{
+			unansweredQuestions = questions.ToList<Question>();
+		}	
+		setCurrentQuestion();
+		//Debug.Log(currentQuestion.fact + " is " + currentQuestion.isTrue);
+	}	
+	void setCurrentQuestion()
+	{
+		int randomQuestionIndex = Random.Range (0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions[randomQuestionIndex];
+		questionText.text = currentQuestion.question;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	IEnumerator TransitionToNextQuestion()
+	{
+		unansweredQuestions.Remove(currentQuestion);
+		yield return new WaitForSeconds(timeBetweenQuestions);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void UserSelectTrue()
+	{
+		if(currentQuestion.option1 == currentQuestion.answer)
+		{
+            Debug.Log("Correct!");
+		}else{
+            Debug.Log("Wrong!");
+		}
+		StartCoroutine(TransitionToNextQuestion());
+	}
+
+	public void UserSelectFalse()
+	{
+		if(currentQuestion.option2 == currentQuestion.answer)
+		{
+            Debug.Log("Correct!");
+		}else{
+            Debug.Log("Wrong!");
+		}
+		StartCoroutine(TransitionToNextQuestion());
 	}
 }
---
+++ Quiz-Game/Assets/Question.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+
+
+public class Question
+{
+    public string question;
+    public string answer;
+    public string option1;
+    public string option2;
+    public string option3;
+    public string option4;
+
+
+    public void QuestionInventory(System.Xml.XmlNode questions)
+    {
+        string question = questions.Attributes["ID"].Value;
+        //	answer = curQuestion.Attributes["Answer"].Value; 
+        string option1 = questions.Attributes["OptionA"].Value;
+        string option2 = questions.Attributes["OptionB"].Value;
+        string option3 = questions.Attributes["OptionC"].Value;
+        string option4 = questions.Attributes["OptionD"].Value;
+
+    }
+}
+
+//Read XML into the files to create the questions and answers!
