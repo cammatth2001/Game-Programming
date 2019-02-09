9defe08a617e9444624c1bd9e960d22637cc9ac1
cammatth2001 <46729770+cammatth2001@users.noreply.github.com>
2019-02-09 08:57:19 -0600

    Spanish Game Creation
    Added UI, updated button assignment, imported the data from csv file.

---
+++ SpanishGame/Assets/QuestionLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        List<Question> game = new List<Question>();
+
+        TextAsset csvFile = Resources.Load<TextAsset>("Questions");
+
+        string[] data = csvFile.text.Split(new char[] {'\n'});
+
+        Debug.Log(data.Length);
+
+         for (int i = 0; i < data.Length; i++)
+          {
+              string[] row = data[i].Split(new char[] { ',' });
+              Question q = new Question();
+                  q.question = row[0];
+                  q.optionA = row[1];
+                  q.optionB = row[2];
+                  q.optionC = row[3];
+                  q.optionD = row[4];
+                  q.answer = row[5];
+
+                  game.Add(q);
+
+          }
+          foreach (Question q in game)
+          {
+              Debug.Log(q.question);
+          }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ SpanishGame/Assets/Questions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question
+{
+    public string question;
+    public string optionA;
+    public string optionB;
+    public string optionC;
+    public string optionD;
+    public string answer;
+}
