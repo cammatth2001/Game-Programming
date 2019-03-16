8373b89918357966d5c4f86a1116c0f7dd2e2b32
cammatth2001 <46729770+cammatth2001@users.noreply.github.com>
2019-03-16 17:41:55 -0500

    Finished Game
    Added minimap, Zombie Chase obstacle course and a maze for the player to walk through

---
+++ MeshGame/Assets/MapMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMovement : MonoBehaviour
+{
+
+    public Transform player;
+   
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+    }
+
+}
